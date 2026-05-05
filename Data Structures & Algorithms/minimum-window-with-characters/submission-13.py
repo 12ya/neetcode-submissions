1363a1b3a6b3240d3dd1f3de13253e1de4aaccfa
iliya <68940374+12ya@users.noreply.github.com>
2026-05-05 15:06:10 +0900

    Add: minimum-window-with-characters - submission-13

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-13.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        charset, window = {}, {}
+        for c in t:
+            charset[c] = charset.get(c, 0) + 1
+
+        result, resultLength = [-1, -1], float("inf")
+        have, need = 0, len(charset) 
+
+        left = 0
+        for right in range(len(s)):
+            char = s[right]
+            if char in charset:
+                window[char] = window.get(char, 0) + 1
+                if window[char] == charset[char]:
+                    have += 1
+
+            while have == need:
+                if resultLength > right - left + 1:
+                    resultLength = right - left + 1
+                    result = [left, right]
+
+                if s[left] in window:
+                    window[s[left]] -= 1
+                    if window[s[left]] < charset[s[left]]:
+                        have -= 1
+                left += 1
+
+        l, r = result
+        return s[l : r + 1] if resultLength != float("inf") else ""
